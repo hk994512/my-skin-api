787f63e4ed05198370715c51670e47c985ce4ccc
M Ameer Hamza <157797338+hk994512@users.noreply.github.com>
2026-04-19 15:41:58 +0500

    Fix model path for skin disease model

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import requests
 from pathlib import Path
 
-MODEL_PATH = "assets/models/skin_disease_model.tflite"
+MODEL_PATH = "assets/model/skin_disease_model.tflite"
 GDRIVE_FILE_ID = "1qr50bnMKsua4NhxyER_lmzXL8f5t_Ped"
 
 def download_model():
