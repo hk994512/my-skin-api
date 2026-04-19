515ac8fb2ea5027eb5d8be27e209fdebdcccd871
M Ameer Hamza <157797338+hk994512@users.noreply.github.com>
2026-04-19 15:31:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 MODEL_PATH = "assets/model/skin_disease_model.tflite"
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn api:app --host 0.0.0.0 --port $PORT
